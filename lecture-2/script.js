e194043f5cbcf9a5964ebdfaa1fc3ad3466e0f34
Archana Patil <archupatil5958@gmail.com>
2026-02-20 11:21:36 +0530

    Add files via upload

---
+++ lecture-2/script.js
@@ -0,0 +1,16 @@
+// wap to find the maximum of 3 numbers using if else statement
+
+let a=200;
+let b=50;
+let c=30;
+
+if(a>b && a>c) {
+    console.log("a is the maximum number=" +a);
+    
+}
+else if(b>a && b>c){
+    console.log("b is the maximum number=" +b);
+}
+else {
+    console.log("c is the maximum number=" +c);
+}
