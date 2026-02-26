8f2a08452b3e0a3dc8acae0f1e82c142fc5fbc3c
Archana Patil <archupatil5958@gmail.com>
2026-02-26 12:27:49 +0530

    Add files via upload

--- lecture-5/script.js
+++ lecture-5/script.js
@@ -1,167 +1,38 @@
-//wap to find max/min from 5 numbers using ladder if else statement
-let a = 10;
-let b = 20;
-let c = 5;
-let d = 15;
-let e = 25;
+//wap to print n to 1 using for while loop 
 
-if (a > b && a > c && a > d && a > e) {
-    console.log("a is the maximum: ");
-}else if (b > a && b > c && b > d && b > e) {
-    console.log("b is the maximum: ");
-}else if (c > a && c > b && c > d && c > e) {
-    console.log("c is the maximum: ");
-}else if (d > a && d > b && d > c && d > e) {
-   console.log("d is the maximum")
-}else {
-    console.log("e is the maximum: ");
-}   
+//let n=25;
+//while (n>=1){
+   // console.log(n);
+    //n--;
+//}
 
-if (a < b && a < c && a < d && a < e) {
-    console.log("a is the minimum: ");
-}else if (b < a && b < c && b < d && b < e) {
-    console.log("b is the minimum: ");
-}else if (c < a && c < b && c < d && c < e) {
-    console.log("c is the minimum: ");
-}else if (d < a && d < b && d < c && d < e) {
-    console.log("d is the minimum: ");
-}else {
-    console.log("e is the minimum: ");
-    
-}   
-    console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-//wap to find  grade based on percentage of 5 user given marks out of 100 using ladder if else statement
-let marks=85;
-if (marks >= 90 && marks <= 100) {
-    console.log("Grade A");
-}else if (marks >= 80 && marks < 90) {
-    console.log("Grade B");
-}else if (marks >= 70 && marks < 80) {
-    console.log("Grade C");
-}else if (marks >= 60 && marks < 70) {
-    console.log("Grade D");
-}else {
-    console.log("fail");
-}   
+//wap to print 1 to n using for while loop
 
-console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-//wap to calculate electricity bill using ladder if else statement
-let units = 350;
-let billAmount = 0; 
-if (units <= 100) {
-    billAmount = units * 5;
-}
-else if (units > 100 && units <= 200) {
-    billAmount = (100 * 5) + ((units - 100) * 7);
-}
-else if (units > 200 && units <= 300) {
-    billAmount = (100 * 5) + (100 * 7) + ((units - 200) * 10);
-}
-else {
-    billAmount = (100 * 5) + (100 * 7) + (100 * 10) + ((units - 300) * 15);
-}
-console.log("Electricity Bill Amount: " + billAmount);
+//let a=30; 
 
-console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+ //while (a<=1){
+ //console.log(a);
+    //a++;
+ //}
 
-//wap to check number divisible by 7 or not using  ladder if else statement.
-let num = 49;
-if (num % 7 === 0) {
-    console.log(num + " is divisible by 7.");   
-} else {
-    
-    console.log(num + " is not divisible by 7.");
-}   
+ //wap to print only even numbers.
+  //let n=70;            //jha tak even numbers nikalne hai vo likha
+  //while (2<=n){        // 2 se n tak even numbers nikalne hai isliye 2 se start kiya
+  //console.log (n);      //yha pr even numbers print honge
+    //n-=2;             //n ki value 70 hai isliye usme se 2 minus krna hai taki even numbers print ho.
+  //}
 
-console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-//wap to check  number is even or odd using ladder if else statement
-let number = 15;
-if (number % 2 === 0) {
-    console.log(number + " is an even number.");
-} else {
-    console.log(number + " is an odd number.");
-    
-}       
-    console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-//wap to check user is eligible or not using ternary operator
-let age = 18;
-let eligibility = (age >= 18) ? "Eligible" : "Not Eligible";
-console.log("User is: " + eligibility);
+  //wap to print only odd numbers.
+  //let m=1;
+   //let n=60;                     //jha tak odd numbers nikalne hai vo likha 
+  //while (m<=n){                  // m mein jo value hai vo n se chhota ya brabr hona chhiye
+   //console.log(m);               // yha pr odd numbers print honge 
+  // m+=2;                         //m ki value 1 hai iske liye usme 2 add  krna hai taki odd numbers print ho.
+  //}
 
-
-console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-//wap to check number is neutral, negative and positive using ternary operator.
-let num1 = -5;
-let result = (num1 > 0) ? "Positive" : (num1 < 0) ? "Negative" : "Neutral";
-console.log(num1 + " is a " + result + " number.");
-
-console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-//wap to display month name by count of month using switch case statement
-let monthNumber = 4;
-let monthName;
-switch (monthNumber) {
-    case 1:
-        monthName = "January";
-        break;
-    case 2:
-        monthName = "February";
-        break;
-    case 3:
-        monthName = "March";
-        break;
-    case 4:
-        monthName = "April";
-        break;
-    case 5:
-        monthName = "May";
-        break;
-    case 6:
-        monthName = "June";
-        break;
-    case 7:
-        monthName = "July";
-        break;
-    case 8:
-        monthName = "August";
-        break;
-    case 9:
-        monthName = "September";
-        break;
-    case 10:
-        monthName = "October";
-        break;
-    case 11:
-        monthName = "November";
-        break;
-    case 12:
-        monthName = "December";
-        break;
-    default:
-        monthName = "Invalid month number";
-}
-console.log("Month Name: " + monthName);
-
-console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-//wap to display message based on grade using switch case statement
-let grade = 'B';
-let message;
-switch (grade) {
-    case 'A':
-        message = "Excellent!";
-        break;
-    case 'B':
-        message = "Good";
-        break;
-    case 'C':
-        message = "Well done";
-        break;
-    case 'D':
-        message = "poor";
-        break;
-    case 'F':
-        message = "Better luck next time!";
-        break;
-    default:
-        message = "Invalid grade";
-}
-console.log("Grade: " + grade + " - " + message);
+  //wap to print only leap years from 2000to 2026.
+  let year=2000; //leap year likho 
+  while (year<=3000){ //jo year tak leap year find krna hai vo likho 
+   console.log(year); //yha pr print hoga vo sab leap year.
+   year+=4; //leap year har 4 saal me aata hai isliye 4 add kiya.
+  }
