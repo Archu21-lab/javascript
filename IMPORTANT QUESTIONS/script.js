5d6393a978b56d61df257b6d8494315e1281b4cb
Archana Patil <archupatil5958@gmail.com>
2026-02-28 16:03:17 +0530

    Add files via upload

---
+++ IMPORTANT QUESTIONS/script.js
@@ -0,0 +1,26 @@
+//wap to  print numbers 1 to N using loop,do...while loop,for loop.
+
+//let n=30;
+//let i=1;
+//while(n>=i){
+  //  console.log(n);
+    //i++;
+  //}
+
+
+  //do while loop
+
+
+
+
+
+
+//for loop
+
+
+let m=30;
+
+for(j=1; m>=j; j++){
+   console.log(m)
+
+}
