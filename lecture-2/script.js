3100e27d9b2d401f91311f871b68148b42fb4784
Archana Patil <archupatil5958@gmail.com>
2026-03-09 15:10:09 +0530

    Comment out multiple JavaScript programs

--- lecture-2/script.js
+++ lecture-2/script.js
@@ -1,16 +1,189 @@
 // wap to find the maximum of 3 numbers using if else statement
 
-let a=200;
-let b=50;
-let c=30;
+// let a=200;
+// let b=50;
+// let c=30;
 
-if(a>b && a>c) {
-    console.log("a is the maximum number=" +a);
+// if(a>b && a>c) {
+//     console.log("a is the maximum number=" +a);
     
-}
-else if(b>a && b>c){
-    console.log("b is the maximum number=" +b);
-}
-else {
-    console.log("c is the maximum number=" +c);
+// }
+// else if(b>a && b>c){
+//     console.log("b is the maximum number=" +b);
+// }
+// else {
+//     console.log("c is the maximum number=" +c);
+// }
+
+//write a javascript program to swap two numbers using a third variable.
+
+// let a=20;
+// let b=40;
+// let c;
+// c=a;
+// a=b;
+// b=c;
+
+//  console.log("a=",a);
+//  console.log("b=",b);
+
+
+//write a javascript program to swap two numbers  without using a third variable.
+
+
+// let a= 60;
+// let b=40;
+
+// a=a+b;
+// b=a-b;
+// a=a-b;
+
+// console.log("a=",a);
+// console.log("b=",b);
+
+
+//write   a javascript program fahrniet tempreture to celcius.
+
+// let fahrenheit=50;
+// let celsius= (fahrenheit-32)*5/9;
+
+// console.log("fahrentheit",celsius);
+
+
+//write a program area of rectangle usingb length and width.
+
+// let length=30;
+// let width= 70;
+
+// let areaofrectangle=width*length;
+
+// console.log("area of rectangle", areaofrectangle);
+
+//write a javascript program to calculate the area of a circle  using the formula pi*r*2*2.
+
+// let radius=5;
+// let areaofcircle=Math.PI*radius*radius;
+
+// console.log("area of circle", areaofcircle);
+
+
+//WRITE A PROGRAM TO CALCULATE THE SIMPLE INTREST USING THE FORMULA.
+
+    // let p=4;
+    // let r=8;
+    // let t=10;
+
+    // let si=(p*r*t)/100;
+
+    // console.log("si=",si);
+
+//write a javascript program to calculate the perimeter of a rectangle.
+
+// let length=20;
+// let width=30;
+
+// let p=2*(length+width);
+
+// console.log("P=",p);
+
+//write a javascript program  to calculate the square of a number.
+
+// let a=100;
+
+// let squareofnumber=a*a;
+
+//  console.log("Square Of Number=",squareofnumber);
+
+//write a javascript program  to calculate the cube of number 
+
+// let a =3;
+// let cube=a*a*a;
+
+// console.log("Cube=",cube);
+
+
+//write a javascript program  to find  the reminder when one number is divided by another.
+// let  a=5;
+// let b=35;
+
+// let divide=b/a;
+
+// console.log("Reminder=",divide);
+
+
+//write a javascript program  to convert minutes into seconds.
+
+// let a=20;
+
+// let seconds= a*60;
+
+// console.log( a+ "min=",seconds+ "seconds");
+
+
+//write a javascript program  to convert hours into minute.
+
+// let a=70;
+// let hours=a*60;
+
+// console.log(a+"hours", hours+ "minutes")
+
+//write a javascript program  to average of three numbers.
+
+// let a=80;
+// let b=20;
+// let c=10;
+
+// let average=a+b+c;
+
+// console.log("Average Of Three Numbers=", average);
+
+//write a javascript program  to kilometer into meters.
+
+// let kilometer=32;
+// let meters=kilometer*1000;
+
+// console.log(kilometer+ "kilometer=", meters );
+
+//write a javascript program  to meters into centimeter.
+
+// let meter=250;
+// let centimeter=meter*100;
+
+// console.log(meter+"meter=",centimeter);
+
+//write a javascript program to calculate total marks and percentage of 6 marks.
+
+// let guj=20;
+// let math=30;
+// let english=40;
+// let sci=30;
+// let ss=70;
+
+// let per=guj+math+english+sci+ss/6;
+
+// console.log("percentage=",per); 
+
+//write a javascript program  to increment a number using increment operator.
+
+// let a=5;
+// a++;
+// console.log("increment number",a);
+
+
+//write a javascript program  to decrement a number using decrement operator.
+
+// let a=6;
+// a--;
+// console.log("decrement number",a);
+
+//write  a javascript program to check if  a number is even or odd using the  modulus operator.
+
+let a=7;
+
+if(a%2==0){
+
+    console.log("This Is Even Number")
+
+}else{
+    console.log("This Is Odd Number")
 }
