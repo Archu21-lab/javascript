39e9d05a6df9e480153675b1ed0894e7ac94eb6c
Archana Patil <archupatil5958@gmail.com>
2026-05-08 11:22:58 +0530

    Update script.js

---
+++ Task Manager/script.js
@@ -0,0 +1,11 @@
+const input_task_text = document.getElementById("input-task-text");
+const input_task_prority= document.getElementById("input-task-prority");
+const input_add_button = document.getElementById("input-add-button");
+
+
+const input_filter_text = document.getElementById("input-filter-text");
+const input_filter_prority = document.getElementById("input-filter-prority");
+const input_filter_status = document.getElementById("input-filter-status");
+const input_filter_button = document.getElementById("input-filter-button");
+
+const task_display_box = document.getElementById("task-display-box");
